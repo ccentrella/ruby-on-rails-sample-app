b22ea93eee70371530110da7223c421545a4665a
Christopher Centrella <ccentrella.j@gmail.com>
2021-12-30 15:11:22 +0000

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    user_params = params.require(:user).permit(:name,
+    :email, :password, :password_confirmation)
+    
+    @user = User.new(user_params) # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      # Reset form
+      render 'new'
+    end
   end
 end
