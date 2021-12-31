4e0cf736aff87a76a58bed382f6e6d7c4e6ce28e
Christopher Centrella <ccentrella.j@gmail.com>
2021-12-31 21:32:04 +0000

    Added functionality for updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,6 @@ class UsersController < ApplicationController
   end
   
   def create
-    user_params = params.require(:user).permit(:name,
-    :email, :password, :password_confirmation)
     
     @user = User.new(user_params) # Not the final implementation!
     if @user.save
@@ -21,4 +19,24 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                                  :password_confirmation)
+    end
 end
