94a4863ead4d3aff68bb6ba0a27eb64af9407816
Christopher Centrella <ccentrella.j@gmail.com>
2022-01-01 19:22:12 +0000

    Fixed error in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    :password => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
